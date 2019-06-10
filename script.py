45d99c0d94d08a50b46770848eafd72284d93baf
Rana Awais <rana.awais@ebryx.com>
2019-06-10 15:14:52 +0500

    remove redundant params.

--- script.py
+++ script.py
@@ -30,16 +30,8 @@ def get_params():
     parser = argparse.ArgumentParser()
     parser.add_argument('--gtoken', default=os.environ.get('ACCESS_TOKEN'),
                         help='Github access token for REST API.')
-    parser.add_argument('--conf', default=os.environ.get('CONFIG_FILE'),
-                        help='config file for program.')
     parser.add_argument('--search', help='Keyword to search in Github\'s '
                         'all repo code.', action='append')
-    parser.add_argument('--json', dest="output_json",
-                        action="store_true", help="Output in JSON")
-    parser.add_argument("--show-regex", action="store_true",
-                        help="prints out regexes that will computed against repo")
-    parser.add_argument("--regex", dest="do_regex",
-                        action="store_true", help="Enable high signal regex checks")
     parser.add_argument("--rules", dest="rules", default=str(),
                         help="Ignore default regexes and source from json list file")
     parser.add_argument("--add-rules", default=str(),
@@ -52,18 +44,16 @@ def get_params():
                         help="User defined entropy threshold for base64 strings [default: 4.5]")
     parser.add_argument("--entropy-hex-thresh", type=float, default=3,
                         help="User defined entropy threshold for hex strings [default: 3.0]")
-    parser.add_argument("--since-commit", dest="since_commit",
-                        default=None, help="Only scan from a given commit hash")
-    parser.add_argument("--max-depth", dest="max_depth", default=1000000,
-                        help="The max commit depth to go back when searching for secrets")
-    parser.add_argument("--branch", dest="branch", default=str(),
-                        help="Name of the branch to be scanned")
-    parser.add_argument("--repo-path", type=str, dest="repo_path", default=str(),
-                        help="Path to the cloned repo. If provided, git_url will not be used")
-    parser.add_argument("--cleanup", dest="cleanup", action="store_true",
-                        help="Clean up all temporary result files")
 
     args = parser.parse_args()
+    setattr(args, 'max_depth', 1000000)
+    setattr(args, 'branch', str())
+    setattr(args, 'since_commit', None)
+    setattr(args, 'repo_path', str())
+    setattr(args, 'cleanup', True)
+    setattr(args, 'do_regex', True)
+    setattr(args, 'show_regex', False)
+    setattr(args, 'output_json', False)
 
     if not args.search:
         logger.info('No search keywork provided. Use --search '
@@ -73,13 +63,6 @@ def get_params():
         logger.info('Github access token not provided. Either specify it '
                     'using --gtoken param or use `ACCESS_TOKEN` env variable.')
         exit(1)
-    if not args.conf:
-        logger.info('Config file not provided. Either specify it using '
-                    '--conf param or use `CONFIG_FILE` env variable.')
-        exit(1)
-    if not os.path.isfile(args.conf):
-        logger.info('Config file not found: %s', args.conf)
-        exit(1)
 
     update_headers(args)
     entropy_options = validate_args(args)
@@ -87,6 +70,8 @@ def get_params():
 
 
 def search_code(keywords):
+
+    global HEADERS
     url = API_BASE + '/search/code?q=%s' % ('|'.join(keywords))
     logger.info('Searching: %s', url)
 
@@ -122,7 +107,6 @@ def run_foresthog(args, options, code):
     logger.info('Processing repo: %s', url)
     setattr(args, 'git_url', url)
     response = process_repo(args, options, False)
-    print(response)
 
 
 if __name__ == "__main__":
